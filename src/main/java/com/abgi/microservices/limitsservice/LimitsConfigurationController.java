07c76cd1116c1c229797893d3acee6a7207fb30e
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2019-11-12 21:20:33 -0300

    Atualizacao

--- src/main/java/com/abgi/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/abgi/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,7 @@
 package com.abgi.microservices.limitsservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,16 @@ import com.abgi.microservices.limitsservice.bean.LimitConfiguration;
 @RestController
 public class LimitsConfigurationController {
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private Configuration configuration;
 
 	@GetMapping("/limits")
 	public LimitConfiguration  retrieveLimitsFromConfiguration() {
+		
+		logger.info("{}", configuration.getMaximum());
+		
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 	
--- src/main/java/com/abgi/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/abgi/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,6 +3,9 @@ package com.abgi.microservices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,8 @@ public class LimitsServiceApplication {
 		SpringApplication.run(LimitsServiceApplication.class, args);
 	}
 
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
