bff2aa1a06a2f62d1f5da74c56ef0a85ec66430b
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2019-11-13 00:40:19 -0300

    Atualizacao

--- src/main/java/com/abgi/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/abgi/microservices/limitsservice/LimitsConfigurationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abgi.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -24,4 +25,13 @@ public class LimitsConfigurationController {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")	
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
+	}
 }
